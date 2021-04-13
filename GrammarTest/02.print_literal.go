ea01d2c7ab2a1b85ccd92809f6b868e1242ee9d5
terzeron <terzeron@gmail.com>
2021-04-13 15:00:22 +0900

    add some test codes for literals

--- GrammarTest/02.print_literal.go
+++ GrammarTest/02.print_literal.go
@@ -7,4 +7,10 @@ import (
 
 func main() {
 	fmt.Println("Happy", math.Pi, "Day")
+	fmt.Println("go" + "lang")
+	fmt.Println("1+1=", 1+1)
+	fmt.Println("7.0/3.0=", 7.0/3.0)
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
 }
